5b0f8b5a40ec30bedeef0dc54e446493a3006fbb
luisomoreau <luis.omoreau@gmail.com>
2016-06-06 11:42:53 +0200

    Added arduino code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# sigfox_connected_target
+# Sigfox Connected Target
+
+##Description
+
+Arduino code for the Instructable tutorial Sigfox Connected Target for Nerfs.
+
+It shows how to send a Sigfox message using the airboard with sigfox shield SigBee (Atim module) when the connected target has been hit. 
+
+
+##Get hardware
+[The AirBoard](http://sales.theairboard.cc/)
+
+[Atim SigBee shield](http://www.atim.com/fr/produits/catalogue/modules-shields/sigbeelorabee/)
+
+Note: The AirBoard has been designed to be wirelessly reprogrammable. The microUSB connector is used for battery charging only. To upload a new program, you will need additional wireless shields such as XBee or BLE and their associated USB dongle, or an external serial connection over FTDI.
+
+##Installation
+
+Install Arduino IDE
+
+Get the Arduino library from [API for ARM modules](http://atim-radiocommunications.github.io/armapi/)
+
+Copy/Past the armapi folder under Arduino/libraries
+
+##Run the code
+
+- plug the light sensor between the A4 and GND pins
+- repeatedly read the light sensor from analog pin A4
+- send the value to the SIGFOX network every 10 minutes
+  (maximum 140 messages per day)
+   
+1. upload this program to The AirBoard via the BLE-LINK/XBee shield
+2. replace the BLE-LINK/XBee shield by the SIGFOX shield
+3. connect to the SIGFOX backend: http://backend.sigfox.com
---
+++ sigfox_connected_target.ino
@@ -0,0 +1,156 @@
+
+
+// ---------------------------------------------------------------------
+// This file is a example for use the library armapi with
+// The AirBoard using the SigBee shield module
+//
+// This software send periodically the analog value of the pin A4 to Sigfox network.
+//
+// Created date: 03.05.2016
+// Author: Louis Moreau
+// Adapted from: https://github.com/atim-radiocommunications/armapi
+// ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
+// Include
+// ---------------------------------------------------------------------
+#include <arm.h>
+#include <TheAirBoard.h>
+// ---------------------------------------------------------------------
+// Define
+// ---------------------------------------------------------------------
+#define GREEN   5             // GREEN LED
+#define BLUE   6              // BLUE LED
+#define RED      9            // RED LED
+#define PIEZO  A4             //Analog Pin 
+#define TIMEINTERVALL 900000  // 900 sec -> 15 min sleep time
+
+// ---------------------------------------------------------------------
+// Global variables
+// ---------------------------------------------------------------------
+//Instance of  the class Arm
+Arm SigFox;
+
+//Instance of the air board class
+TheAirBoard board;
+
+
+//The message to send at sigfox
+uint8_t msg[] = "Hit";
+unsigned long timer;          // timer for sleeping
+bool blue = false;
+bool green = false;
+bool red = false;
+int success;
+int sensorValue;
+bool sigfoxOK = false;
+
+
+// ---------------------------------------------------------------------
+// Implemented functions
+// ---------------------------------------------------------------------
+
+void setup()
+{
+  //Pin modes
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(PIEZO, INPUT);
+
+  resetLeds();
+	//Init Arm and set LED to on if error
+	if (SigFox.Init(&Serial) != ARM_ERR_NONE){
+		digitalWrite(RED, HIGH);
+    
+	}else{
+    digitalWrite(GREEN, HIGH);
+    sigfoxOK=true;
+	}
+	
+	//Set Sigfox mode in uplink.
+	SigFox.SetMode(ARM_MODE_SFX);
+	SigFox.SfxEnableDownlink(false);
+	SigFox.UpdateConfig();
+  delay(1000);
+  resetLeds();
+}
+
+void loop()
+{ 
+  sensorValue = analogRead(PIEZO);
+  //blinkBlue();
+  if(sensorValue==1023){
+    digitalWrite(BLUE,HIGH);
+    delay(200);
+    sendMessage();
+  }else{
+    digitalWrite(BLUE,LOW);
+  }
+//	if (millis()>timer){               // if timer expired
+//   sendMessage(); 
+//   timer=millis()+TIMEINTERVALL;    //reset timer
+//  }  
+}
+
+void sendMessage(){
+  resetLeds();
+  if(sigfoxOK){    
+    float battery = board.batteryChk();
+    delay(1000);
+    for(int i=0;i<60;i++){
+      blinkBlue();
+      delay(200);
+    }
+    //Send the message to sigfox
+    success = SigFox.Send(msg, sizeof(msg));
+    resetLeds();
+    if(success!=sizeof(msg)){
+      digitalWrite(RED, HIGH);
+    }else{
+      digitalWrite(GREEN, HIGH);
+    }
+  }else{
+    digitalWrite(RED, HIGH);
+  }
+  delay(1000);
+  resetLeds();
+}
+
+void blinkBlue(){
+  if(blue){
+    digitalWrite(BLUE, LOW);
+    blue = false;
+  }else{
+    digitalWrite(BLUE, HIGH);
+    blue = true;
+  }
+}
+
+void blinkGreen(){
+  if(green){
+    digitalWrite(GREEN, 0);
+    green = false;
+  }else{
+    analogWrite(GREEN, 1);
+    green = true;
+  }
+}
+
+void blinkRed(){
+  if(red){
+    digitalWrite(RED, 0);
+    red = false;
+  }else{
+    analogWrite(RED, 1);
+    red = true;
+  }
+}
+
+void resetLeds(){
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+  digitalWrite(RED, LOW);
+}
+
+
